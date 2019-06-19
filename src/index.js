95344cd983dd64a4f0ed40777fc5a3c71542fb93
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-06-19 18:12:49 +0100

    commit: a copy from instagram backend: upload, like and making picture available

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+mongoose.connect(
+  "mongodb+srv://jaygibran:mongodb123@cluster0-xjuqh.mongodb.net/insta?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+app.use(cors());
+
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+
+app.use(require("./routes"));
+
+server.listen(3333);
